8dcc0a090486e013cccb626ea3110099f5be3dff
Toshiyuki Shimono <bin4tsv@gmail.com>
2023-03-05 23:21:38 +0900

    Chages digitdemog.pm に加筆。0.084 を作るため。

--- digitdemog.pm
+++ digitdemog.pm
@@ -1,6 +1,6 @@
 package App::digitdemog ; 
-our $VERSION = '0.083' ; 
-our $DATE = '2023-03-05T22:28+09:00' ; 
+our $VERSION = '0.084' ; 
+our $DATE = '2023-03-05T23:20+09:00' ; 
 
 =encoding utf8
 
